24f1a2ce6862c331435d0741d36522e4b47bdc3c
John Adeyemo <ajohnofficial2020@gmail.com>
2025-03-15 05:56:57 -0500

    Debugging reloading pages

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,11 +10,13 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'X-RapidAPI-Key': '623a3ffc47msh15c744f310e655ap1973dbjsn7a585f344adc',
+      'X-RapidAPI-Key': 'd7058db69fmshb3a25cf3abc9b90p176798jsn527a6b8ca932', //#alternate 623a3ffc47msh15c744f310e655ap1973dbjsn7a585f344adc 
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     },
     params: { ...query },
+    
   };
+  console.log(options);
 
   const fetchData = async () => {
     setIsLoading(true);
@@ -34,7 +36,7 @@ const useFetch = (endpoint, query) => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [endpoint, JSON.stringify(query)]);
 
   const refetch = () => {
     setIsLoading(true);
